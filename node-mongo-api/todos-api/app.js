dfbb100d8ec2929ee1bbb589b85c0f49d4e3de5b
Nicole <nicole.sattler@live.com>
2017-12-10 11:37:22 -0600

    Required mongoose, body-parser, set up route and configs

--- node-mongo-api/todos-api/app.js
+++ node-mongo-api/todos-api/app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var todoRoutes = require('./routes/todos');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/api/todos', todoRoutes);
 
 app.get('/', function(req, res){
-    res.send('Hello, World!');
+    res.send('Hello, World! - From root route');
 });
 
 app.listen(3000, function(){
-    console.log('sever started...');
+    console.log('server started...');
 });
