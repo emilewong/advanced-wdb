b2433069c338b9bf8672856785b8bc0d31e62658
Nicole <nicole.sattler@live.com>
2017-12-11 13:29:02 -0600

    Added show, update and delete routes. Added comments

--- node-mongo-api/todos-api/routes/todos.js
+++ node-mongo-api/todos-api/routes/todos.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models');
 
+//INDEX
 router.get('/', function (req, res) {
     db.Todo.find()
         .then(function (todos) {
@@ -12,6 +13,7 @@ router.get('/', function (req, res) {
         })
 });
 
+//CREATE
 router.post('/', function(req, res){
     db.Todo.create(req.body)
     .then(function(newTodo){
@@ -21,5 +23,39 @@ router.post('/', function(req, res){
         res.send(err);
     })
 });
+
+//SHOW
+router.get('/:todoId', function(req, res){
+    db.Todo.findById(req.params.todoId)
+    .then(function(todo){
+        res.json(todo)
+    })
+    .catch(function(err){
+        res.send(err);
+    })
+});
+
+//UPDATE
+router.put('/:todoId', function(req, res){
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+    .then(function(todo){
+        res.json(todo)
+    })
+    .catch(function(err){
+        res.send(err)
+    })
+
+});
+
+//DELETE
+router.delete('/:todoId', function(req, res){
+    db.Todo.findOneAndRemove({_id: req.params.todoId})
+    .then(function(){
+        res.json({message: 'Todo Deleted!'})
+    })
+    .catch(function(err){
+        res.send(err)
+    })
+})
 
 module.exports = router;
