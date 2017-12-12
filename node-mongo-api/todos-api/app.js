7cd39088913b26933b2fa530f1516349ce7354cb
Nicole <nicole.sattler@live.com>
2017-12-12 12:16:39 -0600

    Configured to use public and views directories. Root route sends index.html

--- node-mongo-api/todos-api/app.js
+++ node-mongo-api/todos-api/app.js
@@ -5,10 +5,12 @@ var bodyParser = require('body-parser');
 var todoRoutes = require('./routes/todos');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 app.use('/api/todos', todoRoutes);
 
 app.get('/', function(req, res){
-    res.send('Hello, World! - From root route');
+    res.sendFile('index.html');
 });
 
 app.listen(3000, function(){
