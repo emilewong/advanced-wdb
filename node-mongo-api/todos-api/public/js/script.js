7a84ace8215b87e570eb202ff3c4db888b8f7f08
Nicole <nicole.sattler@live.com>
2017-12-13 11:59:37 -0600

    Added delete functionality

--- node-mongo-api/todos-api/public/js/script.js
+++ node-mongo-api/todos-api/public/js/script.js
@@ -7,6 +7,12 @@ $(function(){
             postTodo($(this).val());
         }
     });
+
+    $('.list').on('click', 'span', function(){
+        var id = $(this).parent().data('id');
+
+        deleteTodo($(this).parent());
+    });
 });
 
 function displayTodos(todos) {
@@ -14,7 +20,8 @@ function displayTodos(todos) {
 }
 
 function makeTodo(todo){
-    var $li = $('<li class="task">' + todo.name + '</li>');
+    var $li = $('<li class="task">' + todo.name + '<span>X</span></li>');
+    $li.data('id', todo._id)
 
     if(todo.completed) {
         $li.addClass('done');
@@ -32,6 +39,21 @@ function postTodo(text) {
     .then(function(todo){
         makeTodo(todo);
         $('#todoInput').val('');
+    })
+    .catch(function(err){
+        console.log(err)
+    });
+}
+
+function deleteTodo(todo) {
+    var id = todo.data('id');
+    $.ajax({
+        method: 'DELETE',
+        url: '/api/todos/'+id
+    })
+    .then(function(data){
+        console.log(data);
+        todo.remove();
     })
     .catch(function(err){
         console.log(err)
